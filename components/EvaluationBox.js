72ce65bde6e1a1b7196013481beefb7390305668
Vinicius Cole de Amorim <viniamorim87@hotmail.com>
2023-03-11 17:48:06 -0300

    evaluations added

---
+++ components/EvaluationBox.js
@@ -0,0 +1,85 @@
+import styles from '../styles/components/EvaluationBox.module.css'
+
+import Image from 'next/image'
+
+import fullStar from '../assets/fullStar.png'
+import emptyStar from '../assets/emptyStar.png'
+import netflixIcon from '../assets/netflixIcon.png'
+import primeVideoIcon from '../assets/primeVideoIcon.png'
+import HBOMaxIcon from '../assets/HBOMaxIcon.png'
+import disneyPlusIcon from '../assets/disneyPlusIcon.png'
+import starPlusIcon from '../assets/starPlusIcon.png'
+import claroTVIcon from '../assets/claroTVIcon.png'
+import piratexIcon from '../assets/piratexIcon.png'
+
+
+export function EvaluationBox({profilePic, username, evaluation, rating, watchedOn = 0}){
+
+    let watchedOnComponent = null;
+    switch (watchedOn) {
+        case 0:
+            watchedOnComponent = (
+                <p className={styles.watchedOnComponent}>NETFLIX <Image src={netflixIcon} alt="netflix icon" /></p>
+            )
+            break;
+        case 1:
+            watchedOnComponent = (
+                <p className={styles.watchedOnComponent}>AMAZON <Image src={primeVideoIcon} alt="prime video icon" /></p>
+            )
+            break;
+        case 2:
+            watchedOnComponent = (
+                <p className={styles.watchedOnComponent}>HBO <Image src={HBOMaxIcon} alt="hbo max icon" /></p>
+            )
+            break;
+        case 3:
+            watchedOnComponent = (
+                <p className={styles.watchedOnComponent}>DISNEY <Image src={disneyPlusIcon} alt="disney plus icon" /></p>
+            )
+            break;
+        case 4:
+            watchedOnComponent = (
+                <p className={styles.watchedOnComponent}>STAR PLUS <Image src={starPlusIcon} alt="star plus icon" /></p>
+            )
+            break;
+        case 5:
+            watchedOnComponent = (
+                <p className={styles.watchedOnComponent}>CLARO <Image src={claroTVIcon} alt="claro tv icon" /></p>
+            )
+            break;
+        case 6:
+            watchedOnComponent = (
+                <p className={styles.watchedOnComponent}>PIRATEX <Image src={piratexIcon} alt="piratex icon" /></p>
+            )
+            break;
+        default:
+            break;
+    }
+
+    console.log(rating)
+
+    return (
+        <div className={styles.evaluation}>
+            <div className={styles.profilePic}>
+                <Image src={profilePic} alt="profile pic" />
+            </div>
+            <div className={styles.evaluationContent}>
+                <h3>{username}</h3>
+                <p>{evaluation}</p>
+                <section className={styles.detailedEvaluationInfo}>
+                    <div className={styles.evaluationRating}>
+                        {[...Array(rating)].map((_, index) => (
+                            <Image key={index} src={fullStar} alt=""/>
+                        ))}
+                        {[...Array((5-rating))].map((_, index) => (
+                            <Image key={index} src={emptyStar} alt=""/>
+                        ))}
+                    </div>
+                    <div className={styles.watchedOn}>
+                        <h5>WATCHED ON: {watchedOnComponent}</h5>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
+}
--- components/FilmCard.js
+++ components/FilmCard.js
@@ -4,18 +4,25 @@ import ReactDOM from 'react-dom';
 
 import styles from "../styles/components/FilmCard.module.css"
 
+import { EvaluationBox } from "@/components/EvaluationBox";
+
 import viewIcon from "@/assets/viewIcon.png"
 import likeIcon from "@/assets/likeIcon.png"
 import dislikeIcon from "@/assets/dislikeIcon.png"
 import estrelaIcon from "@/assets/estrelaIcon.png"
 import closeIcon from "@/assets/closeIcon.png"
 
+import exempleProfilePic from "@/assets/exempleProfilePic.png"
 
-export function FilmCard({cardImage, filmViews, filmRating, filmTitle, filmLikes, filmDislikes, filmYear, filmGenres, filmQtdSeasons, filmDescription, qtdEvaluations, wasWatched=false}){
+
+export function FilmCard({cardImage, filmViews, filmRating, filmTitle, filmLikes, filmDislikes, filmYear, filmGenres, filmDescription, qtdEvaluations, wasWatched=false}){
 
     const [isExpanded, setIsExpanded] = useState(false)
 
+    let evaluations = [];
+
     const handleClick = () =>{
+        // pegar comentarios via api
         setIsExpanded(true)
     }
 
@@ -53,18 +60,28 @@ export function FilmCard({cardImage, filmViews, filmRating, filmTitle, filmLikes
                             <div className={styles.headerStats}>
                                 <h1>{filmTitle}</h1>
                                 <section className={styles.ratingStats}>
-                                    <p>{filmViews} <Image src={viewIcon} alt="icone vizualizção" /></p>
-                                    <p>{filmRating} <Image src={estrelaIcon} alt="icone estrela" /></p>
+                                    <p style={wasWatched ? {}:{display:'none'}}>{filmViews} <Image src={viewIcon} alt="icone vizualizção" /></p>
+                                    <p style={wasWatched ? {}:{display:'none'}}>{filmRating} <Image src={estrelaIcon} alt="icone estrela" /></p>
                                     <p>{filmLikes} <Image src={likeIcon} alt="ícone like" /></p>
                                     <p>{filmDislikes} <Image src={dislikeIcon} alt="ícone dislike" /></p>
                                 </section>
                             </div>
-                            <p className={styles.detailedInformation}>{filmYear}・{filmGenres}・{filmQtdSeasons} SEASONS</p>
+                            <p className={styles.detailedInformation}>{filmYear}・{filmGenres}</p>
                             <p className={styles.description}>{filmDescription}</p>
-                            <section className={styles.evaluations}>
+                            <section className={styles.evaluations} style={wasWatched ? {}:{display:'none'}}>
                                 <section className={styles.evaluationsHeader}>
                                     <h2>EVALUATIONS</h2>
                                     <p>{qtdEvaluations} PEOPLE</p>
+                                </section>
+                                <section className={styles.evaluationsContent}>
+                                    <EvaluationBox profilePic={exempleProfilePic} username={"JOÃOZINHO PIPOCA"} evaluation={"Achei bem meia boca, mas é bom até, mais ou menos."} rating={3} watchedOn = {0} />
+                                    <EvaluationBox profilePic={exempleProfilePic} username={"JOÃOZINHO PIPOCA"} evaluation={"Achei bem meia boca, mas é bom até, mais ou menos."} rating={3} watchedOn = {1} />
+                                    <EvaluationBox profilePic={exempleProfilePic} username={"JOÃOZINHO PIPOCA"} evaluation={"Achei bem meia boca, mas é bom até, mais ou menos."} rating={5} watchedOn = {2} />
+                                    <EvaluationBox profilePic={exempleProfilePic} username={"JOÃOZINHO PIPOCA"} evaluation={"Achei bem meia boca, mas é bom até, mais ou menos."} rating={1} watchedOn = {3} />
+                                    <EvaluationBox profilePic={exempleProfilePic} username={"JOÃOZINHO PIPOCA"} evaluation={"Achei bem meia boca, mas é bom até, mais ou menos."} rating={1} watchedOn = {4} />
+                                    <EvaluationBox profilePic={exempleProfilePic} username={"JOÃOZINHO PIPOCA"} evaluation={"Achei bem meia boca, mas é bom até, mais ou menos."} rating={1} watchedOn = {5} />
+                                    <EvaluationBox profilePic={exempleProfilePic} username={"JOÃOZINHO PIPOCA"} evaluation={"Achei bem meia boca, mas é bom até, mais ou menos."} rating={1} watchedOn = {6} />
+
                                 </section>
                             </section>
                         </section>
@@ -74,4 +91,5 @@ export function FilmCard({cardImage, filmViews, filmRating, filmTitle, filmLikes
               )}
         </>
     )
+
 }
