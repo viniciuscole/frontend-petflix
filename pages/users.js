19dbd24aa118d9686b124764bcc3e49c37bc27a8
Vinicius Cole de Amorim <viniamorim87@hotmail.com>
2023-04-04 21:24:44 -0300

    minor details added to users page

--- pages/users.js
+++ pages/users.js
@@ -1,5 +1,7 @@
 import styles from '@/styles/pages/Users.module.css'
 
+import { useState } from 'react'
+
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import { UserBox } from '@/components/UserBox'
@@ -7,6 +9,13 @@ import { UserBox } from '@/components/UserBox'
 import exempleProfilePic from '@/assets/exempleProfilePic.png'
 
 export default function Users(){
+
+    const [isRegisteringAllowed, setIsRegisteringAllowed] = useState(true) // pegar do banco de dados
+
+    const handleRegisteringAllowed = () =>{
+        setIsRegisteringAllowed(!isRegisteringAllowed)
+    }
+
     return (
         <div className={styles.users}>
             <Header />
@@ -18,8 +27,12 @@ export default function Users(){
                             <p id={styles.qtdUsers}>0 USERS</p>
                         </span>
                         <span>
+                            {isRegisteringAllowed ? ( 
                             <p>REGISTERING ALLOWED</p>
-                            <p id={styles.changeBtn}>CHANGE</p>
+                            ) : (
+                            <p>REGISTERING NOT ALLOWED</p>
+                            )}
+                            <p id={styles.changeBtn} onClick={handleRegisteringAllowed}>CHANGE</p>
                         </span>
                     </section>
                     <section className={styles.usersConteinerContent}>
