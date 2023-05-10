534701381f76802baa29b5c96cf97cf8a57d1f15
jvmoraiscb <jv.moraiscb@gmail.com>
2023-05-10 18:55:05 -0300

    :heavy_check_mark: finish search page

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,13 +1,41 @@
-import Image from 'next/image'
-import searchIcon from '../assets/searchIcon.png'
+import { useState } from "react"
+import { useRouter } from "next/router"
+import Image from "next/image"
+import searchIcon from "../assets/searchIcon.png"
 
-import styles from '../styles/components/SearchBar.module.css'
+import styles from "../styles/components/SearchBar.module.css"
 
-export function SearchBar({placeholderText = "SEARCH FILM/SERIE"}){
+export function SearchBar({ placeholderText = "SEARCH MOVIE/TV SERIES" }) {
+    const [value, setValue] = useState("")
+    const router = useRouter()
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        router.push(`/search?query=${value}&page=1`)
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            router.push(`/search?query=${value}&page=1`)
+        }
+    }
+
     return (
-        <form noValidate action="" role="search" className={styles.searchBar}>
+        <form
+            onSubmit={handleSubmit}
+            noValidate
+            action=""
+            role="search"
+            className={styles.searchBar}
+        >
             <Image src={searchIcon} alt="ícone de pesquisa" />
-            <input type="text" placeholder={placeholderText} />
+            <input
+                type="text"
+                placeholder={placeholderText}
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+                onKeyDown={handleKeyDown}
+            />
         </form>
     )
 }
