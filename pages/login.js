29144eae02ae5a11e0c8d73acb6020f22e3bd93e
Joao Batista <jpcbatista.2004@gmail.com>
2023-05-18 12:00:00 -0300

    🔒 Creating login page

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,12 @@
 import styles from '@/styles/pages/Login.module.css'
 
 import { LoginBox } from '@/components/LoginBox'
-/*import { PromotionalPanel } from '@/components/PromotionalPanel'*/
+import { PromotionalPanel } from '@/components/PromotionalPanel'
 
 function Login(){
     return(
         <div className={styles.main}>
+            <PromotionalPanel/>
             <LoginBox/>
         </div>
     )
