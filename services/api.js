3234b719b5f1b95f77b304e1f807fb8fb69df611
Luiz Felipe Machado <luizfelmach@gmail.com>
2023-05-18 15:00:47 -0300

    update api ip

--- services/api.js
+++ services/api.js
@@ -3,7 +3,7 @@ import { getToken } from "./cookies"
 
 export const api = axios.create({
     // baseURL: "https://apitopcom.pet.inf.ufes.br/"
-    baseURL: "http://localhost:3232/",
+    baseURL: "http://200.137.66.9/",
     headers: {
         "authorization": getToken()
     }
