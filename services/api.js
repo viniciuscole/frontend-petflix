043fb79c761349eb302aa78386ba8f782d701c89
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-06-21 17:20:50 -0300

    update

--- services/api.js
+++ services/api.js
@@ -2,8 +2,7 @@ import axios from "axios"
 import { getToken } from "./cookies"
 
 export const api = axios.create({
-    // baseURL: "https://apitopcom.pet.inf.ufes.br/"
-    baseURL: "http://200.137.66.9/",
+    baseURL: "https://api-petflix.pet.inf.ufes.br/",
     headers: {
         "authorization": getToken()
     }
